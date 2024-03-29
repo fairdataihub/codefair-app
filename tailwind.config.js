388482baf5b5c4af1fd2f33722be7b6e260fdbcf
slugb0t <wheresdorian@gmail.com>
2024-03-29 16:15:57 -0700

    feat: :sparkles: add image with safari border

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,26 @@ module.exports = {
     extend: {
       fontFamily: {
         'inter': ['"Inter"', ...defaultTheme.fontFamily.sans],
-      }
+      },
+      keyframes: {
+        float: {
+          '0%': {
+            'box-shadow': '0 5px 15px 0px rgba(0, 0, 0, 0.6)',
+            transform: 'translatey(0px)'
+          },
+          '50%': {
+            'box-shadow': '0 25px 15px 0px rgba(0, 0, 0, 0.2)',
+            transform: 'translatey(-20px)'
+          },
+          '100%': {
+            'box-shadow': '0 5px 15px 0px rgba(0, 0, 0, 0.6)',
+            transform: 'translatey(0px)'
+          },
+          animation: {
+            'float-item': 'float 6s ease-in-out infinite'
+          }
+        }
+      },
     },
   },
   plugins: [],
