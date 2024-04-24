fa02571d7c177f96504c9c58a0cdef9b58f3c204
slugb0t <wheresdorian@gmail.com>
2024-04-23 22:42:17 -0700

    wip: :construction: logging webhook deliveries

--- nuxt-app/server/middleware/log.ts
+++ nuxt-app/server/middleware/log.ts
@@ -1,3 +1,8 @@
 export default defineEventHandler((event) => {
     console.log("New request: " + event.node.req.url);
+    console.log("Request headers: " + JSON.stringify(event.node));
+    //console.log("Request payload: " + JSON.stringify(event, null, 2));
+    //console.log(event.node.res)
+    // console.log("Request payload: " + JSON.stringify(event.node.req.body));
+
   });
