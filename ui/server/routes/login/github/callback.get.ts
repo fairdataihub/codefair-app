02c79717b3d21cc4050e74b0610a6598ad16e472
slugb0t <wheresdorian@gmail.com>
2024-04-25 17:44:56 -0700

    wip: :construction: auth

---
+++ ui/server/routes/login/github/callback.get.ts
@@ -0,0 +1,67 @@
+import { OAuth2RequestError } from "arctic";
+import { generateIdFromEntropySize } from "lucia";
+import clientPromise from "~/server/utils/mongodb";
+
+export default defineEventHandler(async (event) => {
+    const client = await clientPromise;
+    await client.connect();
+
+    const db = client.db();
+	const query = getQuery(event);
+	const code = query.code?.toString() ?? null;
+	const state = query.state?.toString() ?? null;
+	const storedState = getCookie(event, "github_oauth_state") ?? null;
+	if (!code || !state || !storedState || state !== storedState) {
+		throw createError({
+			status: 400
+		});
+	}
+
+	try {
+		const tokens = await github.validateAuthorizationCode(code);
+		const githubUserResponse = await fetch("https://api.github.com/user", {
+			headers: {
+				Authorization: `Bearer ${tokens.accessToken}`
+			}
+		});
+		const githubUser: GitHubUser = await githubUserResponse.json();
+
+		// Replace this with your own DB client.
+        const existingUser = await db.collection("user").findOne({ github_id: githubUser.id });
+
+		if (existingUser) {
+			const session = await lucia.createSession(existingUser.id, {});
+			appendHeader(event, "Set-Cookie", lucia.createSessionCookie(session.id).serialize());
+			return sendRedirect(event, "/");
+		}
+
+		const userId = generateIdFromEntropySize(10); // 16 characters long
+
+		// Replace this with your own DB client.
+        await db.collection("user").insertOne({
+            id: userId,
+            github_id: githubUser.id,
+            username: githubUser.login
+        });
+
+		const session = await lucia.createSession(userId, {});
+		appendHeader(event, "Set-Cookie", lucia.createSessionCookie(session.id).serialize());
+		return sendRedirect(event, "/");
+	} catch (e) {
+		// the specific error message depends on the provider
+		if (e instanceof OAuth2RequestError) {
+			// invalid code
+			throw createError({
+				status: 400
+			});
+		}
+		throw createError({
+			status: 500
+		});
+	}
+});
+
+interface GitHubUser {
+	id: string;
+	login: string;
+}
---
+++ ui/server/utils/auth.ts
@@ -0,0 +1,166 @@
+// import { Lucia } from "lucia";
+// import { MongodbAdapter } from "@lucia-auth/adapter-mongodb"
+// import { Collection } from "mongodb";
+// import clientPromise from "./mongodb"
+
+// const client = await clientPromise;
+// await client.connect();
+
+// const db = client.db();
+// const User = db.collection("users") as Collection<UserDoc>;
+// const Session = db.collection("sessions") as Collection<SessionDoc>;
+
+// const adapter = new MongodbAdapter(Session, User);
+
+// export const lucia = new Lucia(adapter, {
+// 	sessionCookie: {
+// 		attributes: {
+// 			secure: !import.meta.dev
+// 		}
+// 	},
+// 	getUserAttributes: (attributes) => {
+// 		return {
+// 			// attributes has the type of DatabaseUserAttributes
+// 			githubId: attributes.github_id,
+// 			username: attributes.username
+// 		};
+// 	}
+// });
+
+// declare module "lucia" {
+//     interface Register {
+//         Lucia: typeof lucia;
+//         DatabaseUserAttributes: DatabaseUserAttributes;
+//     }
+// }
+
+// interface DatabaseUserAttributes {
+//     github_id: number;
+//     username: string;
+// }
+
+// interface UserDoc {
+//     _id: string;
+// }
+
+// interface SessionDoc {
+//     _id: string;
+//     expires_at: Date;
+//     user_id: string;
+// }
+
+// import { Lucia } from "lucia";
+// import { Collection } from "mongodb";
+// import { MongodbAdapter } from "@lucia-auth/adapter-mongodb";
+// import clientPromise from "./mongodb";
+// import { GitHub } from "arctic";
+
+// interface DatabaseUser {
+// 	id: string;
+// 	username: string;
+// 	github_id: number;
+// }
+
+// interface UserDoc {
+// 	_id: string;
+// }
+
+// interface SessionDoc {
+// 	_id: string;
+// 	expires_at: Date;
+// 	user_id: string;
+// }
+
+
+// // import { webcrypto } from "crypto";
+// // globalThis.crypto = webcrypto as Crypto;
+
+// const client = await clientPromise;
+// await client.connect();
+
+// const db = client.db();
+// const User = db.collection("users") as Collection<UserDoc>;
+// const Session = db.collection("sessions") as Collection<SessionDoc>;
+
+// const adapter = new MongodbAdapter(Session, User);
+
+// export const lucia = new Lucia(adapter, {
+// 	sessionCookie: {
+// 		attributes: {
+// 			secure: !import.meta.dev
+// 		}
+// 	},
+// 	getUserAttributes: (attributes) => {
+// 		return {
+// 			username: attributes.username,
+// 			githubId: attributes.github_id
+// 		};
+// 	}
+// });
+
+// declare module "lucia" {
+// 	interface Register {
+// 		Lucia: typeof lucia;
+// 		DatabaseUserAttributes: Omit<DatabaseUser, "id">;
+// 	}
+// }
+
+// // const config = useRuntimeConfig();
+
+// export const github = new GitHub(process.env.GITHUB_CLIENT_ID!, process.env.GITHUB_CLIENT_SECRET!);
+
+import { Lucia } from "lucia";
+import { MongodbAdapter } from "@lucia-auth/adapter-mongodb";
+import { Collection, MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+    throw new Error("Please add your Mongo URI to .env.local");
+}
+
+const client = new MongoClient(process.env.MONGODB_URI!);
+client.connect();
+
+const db = client.db();
+const User = db.collection("users") as Collection<UserDoc>;
+const Session = db.collection("sessions") as Collection<SessionDoc>;
+
+const adapter = new MongodbAdapter(Session, User);
+
+export const lucia = new Lucia(adapter, {
+	sessionCookie: {
+		attributes: {
+			secure: !import.meta.dev
+		}
+	},
+	getUserAttributes: (attributes) => {
+		return {
+			username: attributes.username,
+			githubId: attributes.github_id
+		};
+	}
+});
+
+declare module "lucia" {
+	interface Register {
+		Lucia: typeof lucia;
+		DatabaseUserAttributes: Omit<DatabaseUser, "id">;
+	}
+}
+
+
+interface DatabaseUser {
+	id: string;
+	username: string;
+	github_id: number;
+}
+
+
+interface UserDoc {
+	_id: string;
+}
+
+interface SessionDoc {
+	_id: string;
+	expires_at: Date;
+	user_id: string;
+}
