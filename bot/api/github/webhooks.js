b0bda500de418c363482515d66473e6260884476
Sanjay Soundarajan <sanjay.16@live.com>
2024-05-31 13:17:59 -0700

    🚚 wip: test path

--- bot/api/github/webhooks.js
+++ bot/api/github/webhooks.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { createNodeMiddleware, createProbot } = require("probot");
 
-const app = require("../../app");
+const app = require("../../esapp");
 
 const probot = createProbot();
 
