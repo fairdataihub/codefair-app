6bf6000c6454e4bc11fe14d92704e8c59b7ca61b
slugb0t <wheresdorian@gmail.com>
2024-06-04 15:38:28 -0700

    test

---
+++ bot/main.js
@@ -0,0 +1,20 @@
+const { Server, Probot } = require("probot");
+const app =  require("./index.js");
+// import "dotenv/config";
+
+async function startServer() {
+  const server = new Server({
+    // log: console,
+    Probot: Probot.defaults({
+      appId: process.env.GITHUB_APP_ID,
+      privateKey: process.env.PRIVATE_KEY,
+      secret: process.env.WEBHOOK_SECRET,
+    }),
+  });
+
+  await server.load(app);
+
+  server.start();
+}
+
+startServer();
