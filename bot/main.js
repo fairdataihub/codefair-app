cf2450b93788f7f59ff3522e7c1a2449e4d23dfe
slugb0t <wheresdorian@gmail.com>
2024-06-06 10:09:34 -0700

    feat: :sparkles: run probot programatically

---
+++ bot/main.js
@@ -0,0 +1,20 @@
+import { Server, Probot } from "probot";
+import app from "./index.js";
+// import "dotenv/config";
+
+async function startServer() {
+  const server = new Server({
+    // log: console,
+    Probot: Probot.defaults({
+      appId: process.env.GITHUB_APP_ID,
+      privateKey: process.env.PRIVATE_KEY,
+      secret: process.env.WEBHOOK_SECRET,
+    }),
+  });
+
+  await server.load(app);
+
+  server.start();
+}
+
+startServer();
