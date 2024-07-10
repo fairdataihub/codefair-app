c028c97da08ef1cabad7d0c010bfd6651b3f29b1
slugb0t <wheresdorian@gmail.com>
2024-07-09 17:03:37 -0700

    wip: :construction: background change

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -19,28 +19,15 @@ const toggleMobileMenu = () => {
       aria-hidden="true"
     >
       <div
-        class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
-      ></div>
+        class="relative aspect-[1155/678] w-full bg-cover bg-no-repeat"
+        style="background-image: url(&quot;/Hexagon.svg&quot;)"
+      >
+        <img
+          src="/Hexagon.svg"
+          alt=""
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
     </div>
 
     <div class="relative z-20 mx-auto w-full max-w-screen-xl px-4 md:px-8">
@@ -258,7 +245,15 @@ const toggleMobileMenu = () => {
                 />
               </g>
             </svg>
-            by the FAIR Data Innovations Hub
+
+            by the&nbsp;
+            <NuxtLink
+              to="https://fairdataihub.org/"
+              target="_blank"
+              class="hover:text-indigo-500"
+            >
+              FAIR Data Innovations Hub
+            </NuxtLink>
           </p>
 
           <br />
