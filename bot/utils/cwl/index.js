1b98a1e618359bf64d05258b0b828130b70e4ef1
slugb0t <wheresdorian@gmail.com>
2024-07-29 15:50:29 -0700

    feat: :sparkles: recursive search for all cwl files in repo

--- bot/utils/cwl/index.js
+++ bot/utils/cwl/index.js
@@ -1,34 +1,40 @@
 export async function checkForCWLFile(context, owner, repo) {
+  const cwlFiles = [];
   console.log("Checking for CWL file");
-  try {
-    const repoContent = await context.octokit.repos.getContent({
-      owner,
-      repo,
-    });
 
-    let cwlFileExists = false;
-    // console.log("repoCONTENT");
-    // console.log(repoContent);``
-    // console.log("repoCONTENT");
+  async function searchDirectory(path) {
+    try {
+      const repoContent = await context.octokit.repos.getContent({
+        owner,
+        path,
+        repo,
+      });
 
-    for (const file of repoContent.data) {
-      const fileSplit = file.name.split(".");
-      // console.log("fileSplit");
-      // console.log(fileSplit);
-      if (fileSplit.includes("cwl")) {
-        console.log("CWL file found");
-        cwlFileExists = true;
-        break;
+      for (const file of repoContent.data) {
+        // const fileSplit = file.name.split(".");
+        // fileSplit.includes("cwl")
+        if (file.type === "file" && file.name.endsWith(".cwl")) {
+          cwlFiles.push(file);
+        }
+        if (file.type === "dir") {
+          await searchDirectory(file.path);
+        }
+      }
+    } catch (error) {
+      console.log("Error getting the repository content");
+      console.log(error);
+      if (error.status === 404) {
+        // Repository is empty
+        return cwlFiles;
       }
     }
+  }
 
-    return cwlFileExists;
+  try {
+    await searchDirectory("");
+    return cwlFiles;
   } catch (error) {
-    console.log("Error getting the repository content");
+    console.log("Error checking for CWL file");
     console.log(error);
-    if (error.status === 404) {
-      // Repository is empty
-      return false;
-    }
   }
 }
