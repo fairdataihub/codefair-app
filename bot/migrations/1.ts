1bb82fdde3893c9b5c2c419dda1a64ea75379f45
Dorian Portillo <wheresdorian@gmail.com>
2024-08-29 16:57:04 -0700

    feat: :sparkles: Migrations update (#63)
    * feat: :sparkles: enable migrations
    
    * feat: :sparkles: add migrate script to bot package.json
    
    * fix: :bug: enable sign in button
    
    * fix: :bug: patch multiple entries during migration

--- bot/migrations/1.ts
+++ bot/migrations/1.ts
@@ -26,15 +26,16 @@ const migrationFunction = async (
   );
 
   // Get all the records where the 'installationId' field exists
-  const installationsWithInstallationId = await installationCollection
+  const installationsWithoutActionCount = await installationCollection
     .find({
       action_count: { $exists: false },
     })
     .toArray();
 
   // Iterate over each record
-  installationsWithInstallationId.forEach(async (installation) => {
+  for (const installation of installationsWithoutActionCount) {
     // Insert the changes we are going to make into the 'migratedData' collection
+    consola.info(`Updating ${installation.repo} entry`);
     await migratedDataCollection.insertOne({
       collection: "installation",
       document_id: installation._id,
@@ -53,11 +54,34 @@ const migrationFunction = async (
         },
       },
     );
-  });
+  }
+  // installationsWithoutActionCount.forEach(async (installation) => {
+  //   // Insert the changes we are going to make into the 'migratedData' collection
+  //   await migratedDataCollection.insertOne({
+  //     collection: "installation",
+  //     document_id: installation._id,
+  //     field: "installation_id",
+  //     migration_id: migrationId,
+  //     text: "Set action_count to 0",
+  //     value: null,
+  //   });
+
+  //   // Update the 'installationId' field to 'installation_id'
+  //   await installationCollection.updateOne(
+  //     { _id: installation._id },
+  //     {
+  //       $set: {
+  //         action_count: 0,
+  //       },
+  //     },
+  //   );
+  // });
 
   consola.success(
     "Add action_count key entries in the 'installation' collection",
   );
+
+  return installationsWithoutActionCount.length;
 };
 
 export default migrationFunction;
