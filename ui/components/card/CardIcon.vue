8b4777ab094c05cae0cf330ff8233adeccb9d94d
slugb0t <wheresdorian@gmail.com>
2024-10-01 12:41:22 -0700

    feat: :sparkles: add check mark to card icon when clicked

--- ui/components/card/CardIcon.vue
+++ ui/components/card/CardIcon.vue
@@ -13,15 +13,22 @@ defineProps({
     type: String,
   },
 })
+
+const showClicked = ref(false);
+
 </script>
 
 <template>
-    <div class="relative flex flex-col items-center justify-center w-40 h-40 border border-gray-300 rounded-lg shadow-md bg-white">
+    <div @click="showClicked = !showClicked" class="relative flex flex-col items-center justify-center w-40 h-40 border border-gray-300 rounded-lg shadow-md bg-white">
     
       <!-- Edit Icon (NuxtLink) -->
     <NuxtLink :to="editLink" class="absolute top-2 right-2 text-blue-500 hover:text-blue-700">
       <Icon name="mdi:pencil" size="1.5rem" />
     </NuxtLink>
+
+    <div v-if="showClicked" :to="editLink" class="absolute top-2 left-2 text-green-500">
+      <Icon name="mdi:checkbox-marked-circle" size="1.5rem" />
+    </div>
     
     <!-- Main Icon (Slot) -->
     <Icon :name="icon" size="3rem" />
