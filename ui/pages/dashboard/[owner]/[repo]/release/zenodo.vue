d801675fff86ae5a0ba7f51e33f5309e23cdbb75
Sanjay Soundarajan <sanjay.16@live.com>
2024-10-15 14:18:57 -0600

    fix: update zenodo success message

--- ui/pages/dashboard/[owner]/[repo]/release/zenodo.vue
+++ ui/pages/dashboard/[owner]/[repo]/release/zenodo.vue
@@ -642,13 +642,15 @@ onBeforeUnmount(() => {
         <template #content>
           <div class="flex w-full flex-col space-y-4">
             <p class="w-full text-base">
-              Your codemeta.json file is used to generate the
-              title, description, and metadata of your Zenodo deposition. Please
-              make sure that the content of this file is correct and up-to-date. 
+              Your codemeta.json file is used to generate the title,
+              description, and metadata of your Zenodo deposition. Please make
+              sure that the content of this file is correct and up-to-date.
             </p>
 
             <n-alert type="info" class="w-full">
-              Codefair will automatically add/update the version number, last modified date, and software identifier before the release so you do not need to worry about them.
+              Codefair will automatically add/update the version number, last
+              modified date, and software identifier before the release so you
+              do not need to worry about them.
             </n-alert>
 
             <n-checkbox v-model:checked="metadataChecked">
@@ -1126,8 +1128,9 @@ onBeforeUnmount(() => {
 
       <n-flex v-else-if="zenodoPublishStatus === 'published'" vertical>
         <p>
-          Your Zenodo deposition has been published. You can view the Zenodo
-          record on the dashboard.
+          Your software was successfully archived on Zenodo. We recommend
+          reviewing the deposition and adding additional metadata supported by
+          Zenodo to make your software more FAIR.
         </p>
       </n-flex>
 
@@ -1138,7 +1141,7 @@ onBeforeUnmount(() => {
               <template #icon>
                 <Icon name="simple-icons:zenodo" size="16" />
               </template>
-              View Zenodo record
+              View Zenodo deposition
             </n-button>
           </NuxtLink>
 
