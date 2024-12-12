dbd7ab3142ef644a6a27e9e2597883150571743c
slugb0t <wheresdorian@gmail.com>
2024-12-12 13:24:42 -0800

    refactor: :recycle: :loud_sound: replace consola with logwatch

--- bot/main.js
+++ bot/main.js
@@ -6,8 +6,6 @@ const privateKey = process.env.GH_APP_PRIVATE_KEY.replace(/\\n/g, "\n");
 
 async function startServer() {
   logwatch.info("Starting server...");
-  logwatch.info({ prop1: "value1", prop2: "value2" });
-  logwatch.debug({ prop3: "value1", prop4: "value2" }, true);
 
   const server = new Server({
     port: process.env.PORT || 3000,
