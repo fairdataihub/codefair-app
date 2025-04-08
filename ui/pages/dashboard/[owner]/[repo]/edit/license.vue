1018aa04943f0fc22881f7d69b86ee9f2688dbad
slugb0t <wheresdorian@gmail.com>
2025-04-08 14:40:52 -0700

    fix: :bug: update targetblankextension location

--- ui/pages/dashboard/[owner]/[repo]/edit/license.vue
+++ ui/pages/dashboard/[owner]/[repo]/edit/license.vue
@@ -2,7 +2,7 @@
 import sanitizeHtml from "sanitize-html";
 import { MdEditor, config } from "md-editor-v3";
 import licensesJSON from "@/assets/data/licenses.json";
-import TargetBlankExtension from "@/lib/TargetBlankExtension";
+import TargetBlankExtension from "@/utils/TargetBlankExtension";
 import { useBreadcrumbsStore } from "@/stores/breadcrumbs";
 
 config({
