85f27bcf96698949b23145c7a2fa8764bbabda04
slugb0t <wheresdorian@gmail.com>
2025-04-27 18:40:56 -0700

    feat: :sparkles: :memo: jsdoc comment for checkForCompliance fn

--- bot/utils/compliance-checks/index.js
+++ bot/utils/compliance-checks/index.js
@@ -3,6 +3,22 @@ import { checkForCitation } from "../../citation/index.js";
 import { checkForCodeMeta } from "../../codemeta/index.js";
 import { getCWLFiles } from "../../cwl/index.js";
 
+/**
+ * * Check for compliance of a repository with Codefair standards.
+ * @param {Object} context
+ * @param {String} owner
+ * @param {Object} repository
+ * @param {Boolean} fullCodefairRun
+ *
+ * @returns {Object} An object containing the results of the compliance checks.
+ * @property {Boolean} citation - Boolean citation check result.
+ * @property {Boolean} codemeta - Boolean codemeta check result.
+ * @property {Object} cwl - Object containing information about CWL files.
+ * @property {Boolean} cwl.contains_cwl_files - Boolean indicating if CWL files are present.
+ * @property {Array} cwl.files - Array of CWL files found in the repository.
+ * @property {Array} cwl.removed_files - Array of CWL files that were removed.
+ * @property {Boolean} license - Boolean license check result.
+ */
 export async function checkForCompliance(
   context,
   owner,
