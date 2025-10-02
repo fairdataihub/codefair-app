0002e7155af594eae5fb5335e18d1f68944f4850
slugb0t <wheresdorian@gmail.com>
2025-10-02 12:54:36 -0700

    refactor: :recycle: rounded to the nearest hundreth rather then tenth for repo count

--- ui/server/api/utils/stats.get.ts
+++ ui/server/api/utils/stats.get.ts
@@ -7,7 +7,7 @@ export default defineEventHandler(async () => {
       installations.map((installation) => installation.owner),
     );
     let uniqueOwnerCount = uniqueOwners.size;
-    const totalRepoCount = Math.floor(installations.length / 10) * 10;
+    const totalRepoCount = Math.floor(installations.length / 100) * 100;
     // If greater than or equal to 50, round to nearest 10
     if (uniqueOwnerCount >= 50) {
       uniqueOwnerCount = Math.floor(uniqueOwnerCount / 10) * 10;
@@ -18,6 +18,10 @@ export default defineEventHandler(async () => {
     };
   } catch (error) {
     console.error("Error occurred while counting installations:", error);
-    return { error: "Error occurred while counting installations" };
+    return {
+      error: "Error occurred while counting installations",
+      totalRepoCount: 0,
+      uniqueOwnerCount: 0,
+    };
   }
 });
